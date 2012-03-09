b41dd536ba1cd4ced18fbe925172784e467f5d32
mosa <mosa@mosa-virtual-machine.(none)>
2012-03-10 05:01:19 +0900

    mosa_test  enter the commit message for your changes. Lines starting

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ApplicationController
+  def hello
+  end
+
+  def bye
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = 'b96d27c89d0ac35dd043b4add4be1783a9b98994cb9a7e3c0a5745e10f141bba0ef9d1888d749780e65d371b4d198abae7c7fead53768ccdf9eed2e14572dae5'
